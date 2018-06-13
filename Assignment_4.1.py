8b5379ba7b6707c320e33ab6eaa5386dd185e881
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 11:51:37 +0530

    Statistics Assignment 4.1

---
+++ Assignment_4.1.py
@@ -0,0 +1,40 @@
+
+Problem Statement: 4.1
+
+You survey households in your area to find the average rent they are paying. Find the standard deviation from the following data:
+
+1550, 1700, 900, 850, 1000, 950.
+
+
+I Step : To find the standard deviation, we need to find the Mean of all the data.
+
+(1550 + 1700 + 900 + 850 + 1000 + 950)/6 = 1158.33
+
+
+II Step : We have to Subtract the mean from each value. This gives you the differences:
+1550 – 1158.33 = 391.67
+1700 – 1158.33 = 541.67
+900 – 1158.33 = -258.33
+850 – 1158.33 = -308.33
+1000 – 1158.33 = 158.33
+950 – 1158.33 = 208.33
+
+
+III Step : Squareing the differences
+391.672 = 153405.3889
+541.672 = 293406.3889
+-258.332 = 66734.3889
+-308.332 = 95067.3889
+158.332 = 25068.3889
+208.332 = 43401.3889
+
+
+IV Step : Add up all of the squares you found in Step III:
+      And divide by 5 ( 6 – 1)
+(153405.3889 + 293406.3889 + 66734.3889 + 95067.3889 + 25068.3889 + 43401.3889) / 5 = 135416.667
+
+Step IV: Find the square root of the number you found in Step IV (the variance):
+    
+135416.667 = 367.99
+
+The obtained value of  standard deviation  = 367.99.
